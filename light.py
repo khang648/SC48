582e74a07a56958883c5c05b55515b1e86061359
Quach Tan Khang <qtk0608@gmail.com>
2021-12-01 17:21:45 +0700

    update

--- light.py
+++ light.py
@@ -444,7 +444,7 @@ def mainscreen():
             average_value = round(sum(test_list)/len(test_list),1)
             print("average_value:", average_value)
             #thr3l_value =  round((threshold + (average_value - 0)/(ratio1/ratio2)),1)
-            tmp_value = (average_value - raw)//ratio1
+            tmp_value = int((average_value - raw)/ratio1)
             thr2_value =  round((tmp_value*ratio2)+threshold, 1)
             #thr3h_value = thr3l_value + 0.2
             thr1_value = round(thr2_value*90/100, 1)
