cd1cbcf975377208768673fbc0ff7b4547364aed
Quach Tan Khang <qtk0608@gmail.com>
2021-12-02 10:20:57 +0700

    update lại tỉ lệ t2 t3

--- light.py
+++ light.py
@@ -445,10 +445,10 @@ def mainscreen():
             print("average_value:", average_value)
             #thr3l_value =  round((threshold + (average_value - 0)/(ratio1/ratio2)),1)
             tmp_value = int((average_value - raw)/ratio1)
-            thr3l_value =  round((tmp_value*ratio2)+threshold, 1)
+            thr2_value =  round((tmp_value*ratio2)+threshold, 1)
             #thr3h_value = thr3l_value + 0.2
+            thr3l_value = round(thr2_value/1.1, 1)
             thr1_value = thr3l_value - 1.5
-            thr2_value = round(thr3l_value*110/100, 1)
 
             fw0 = open("/home/pi/Spotcheck/ct.txt","w")
             fw0.truncate(0)
